59e15b2d32d5628c99f93faaa24b736478232372
bhare1987 <brandonlhare@gmail.com>
2016-02-10 15:23:46 -0500

    added line breaks

--- kanye.js
+++ kanye.js
@@ -1,14 +1,14 @@
 console.log("Welcome to Jouney to the (Kanye) West!");
 var score = 0,
-    answer1 = prompt("Congratulations! You have been chosen to spend the day with Kanye West. This is a rare honor Mr. West bestows only upon his most loyal subjects. Mr. West sets the itinerary, but he gives you the choice. What do you want to do? 1. Go to the bike store. 2. Go to the Grammys 3. Go get some food.");
+    answer1 = prompt("Congratulations! You have been chosen to spend the day with Kanye West. This is a rare honor Mr. West bestows only upon his most loyal subjects. Mr. West sets the itinerary, but he gives you the choice. What do you want to do? \n\n1. Go to the bike store. \n\n2. Go to the Grammys \n\n3. Go get some food.");
 
 if (answer1 === "1" || answer1 === "Go to the bike store"){
   score += 1;
-  var answer1_1 = prompt("'I make awesome decisions in bike stores!' exclaims Kanye. You jump in his ride and cruise down to the bike store. Racks of bikes of all different shapes, colors, and sizes are yours for the taking. Kanye snaps his fingers to get you back on task. 'You are going to help me find the perfect bike for Kim.' Do you choose 1. The stylish road bike 2. The practical beach cruiser 3. The Big Wheel Tricycle");
+  var answer1_1 = prompt("'I make awesome decisions in bike stores!' exclaims Kanye. You jump in his ride and cruise down to the bike store. Racks of bikes of all different shapes, colors, and sizes are yours for the taking. Kanye snaps his fingers to get you back on task. 'You are going to help me find the perfect bike for Kim.' Do you choose \n\n1. The stylish road bike \n\n2. The practical beach cruiser \n\n3. The Big Wheel Tricycle");
 
   if (answer1_1 === "1" || answer1_1 === "The stylish road bike") {
     score += 1;
-    var answer1_1_1 = prompt("'Nah, I like the other one better,' says Kanye, 'and I know style. I been fly so long I feel asleep on the plane!' He begins to bike around the store. What do you do? 1. Agree with Kanye 2. Stick a bicycle pump in the spokes 3. Ride around with him");
+    var answer1_1_1 = prompt("'Nah, I like the other one better,' says Kanye, 'and I know style. I been fly so long I feel asleep on the plane!' He begins to bike around the store. What do you do? \n\n1. Agree with Kanye \n\n2. Stick a bicycle pump in the spokes \n\n3. Ride around with him");
 
     if (answer1_1_1 === "1" || answer1_1_1 === "Agree with Kanye") {
       score += 5;
@@ -18,7 +18,7 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
       score -= score;
       kanyeGuard();
     } else if (answer1_1_1 === "3"  || answer1_1_1 === "Ride around with him") {
-      score +=5;
+      score += 5;
       console.log("You ride around the store with Kanye and start an impromptu bike race. You let Kanye win. 'See, I told you I make awesome decisions in bike stores!' he says");
       kanyeWin();
     } else {
@@ -27,7 +27,7 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
     };
   } else if (answer1_1 === "2" || answer1_1 === "The practical beach cruiser") {
     score += 1;
-    var answer1_1_2 = prompt("'Nah, I like the other one better,' says Kanye, 'and I know style. I been fly so long I feel asleep on the plane!' He begins to bike around the store. What do you do? 1. Agree with Kanye 2. Stick a bicycle pump in the spokes 3. Ride around with him");
+    var answer1_1_2 = prompt("'Nah, I like the other one better,' says Kanye, 'and I know style. I been fly so long I feel asleep on the plane!' He begins to bike around the store. What do you do? \n\n1. Agree with Kanye \n\n2. Stick a bicycle pump in the spokes \n\n3. Ride around with him");
 
     if (answer1_1_2 === "1" || answer1_1_2 === "Agree with Kanye") {
       score += 5;
@@ -46,7 +46,7 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
     };
   } else if (answer1_1 === "3" || answer1_1 === "The Big Wheel Tricycle") {
       score +=1;
-      var answer1_1_3 = prompt("'You think this is a joke son?!' Kanye demands. How do you respond? 1. Yes, just like your career 2. No way, big wheels are awesome 3. Run away");
+      var answer1_1_3 = prompt("'You think this is a joke son?!' Kanye demands. How do you respond? \n\n1. Yes, just like your career \n\n2. No way, big wheels are awesome \n\n3. Run away");
 
       if (answer1_1_3 === "1" || answer1_1_3 === "Yes, just like your career") {
         score -= score;
@@ -72,11 +72,11 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
 
 } else if (answer1 === "2" || answer1 === "Go to the Grammys") {
     score += 1;
-    var answer1_2 = prompt("'My greatest pain in life is that I will never be able to see myself perform live. But everyone else will. Let's bounce,' says Kanye. You take Kanye's chopper to the Grammys and land on the red carpet. As you make your way to your seats, Kanye sees that Drake has taken his seat. 'I'm about to throw some kazoo on this bitch! You got my back?' Do you 1. Have his back 2. Turn traitor and support Drake 3. Find T-Swift and see if she's dating anyone right now");
+    var answer1_2 = prompt("'My greatest pain in life is that I will never be able to see myself perform live. But everyone else will. Let's bounce,' says Kanye. You take Kanye's chopper to the Grammys and land on the red carpet. As you make your way to your seats, Kanye sees that Drake has taken his seat. 'I'm about to throw some kazoo on this bitch! You got my back?' Do you \n\n1. Have his back \n\n2. Turn traitor and support Drake \n\n3. Find T-Swift and see if she's dating anyone right now");
 
     if (answer1_2 === "1" || answer1_2 === "Have his back") {
       score += 1;
-      var answer1_2_1 = prompt("'Oh hell yeah, let's cut 'em up!' exclaims Kanye. You and Kanye run up on Drake. Things get heated quickly as Kanye grabs Drake and tries to lift him out of his seat. Drake's crew sees the scuffle and run over to help. What do you do? 1. Engage all of Drake's crew in hand to hand combat 2. Run like hell ");
+      var answer1_2_1 = prompt("'Oh hell yeah, let's cut 'em up!' exclaims Kanye. You and Kanye run up on Drake. Things get heated quickly as Kanye grabs Drake and tries to lift him out of his seat. Drake's crew sees the scuffle and run over to help. What do you do? \n\n1. Engage all of Drake's crew in hand to hand combat \n\n2. Run like hell ");
 
       if (answer1_2_1 === "1" || answer1_2_1 === "Engage all of Drake's crew in hand to hand combat") {
         score -= 1;
@@ -91,7 +91,7 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
 
     } else if (answer1_2 === "2" || answer1_2 === "Turn traitor and support Drake") {
       score += 1;
-      var answer1_2_2 = prompt("Kanye does not take your betrayal well as you make your way over to Drake's side. Enraged, he attacks. Do you 1. Engage Kanye in hand to hand combat 2. Push Drake into Kanye 3. Run away");
+      var answer1_2_2 = prompt("Kanye does not take your betrayal well as you make your way over to Drake's side. Enraged, he attacks. Do you \n\n1. Engage Kanye in hand to hand combat \n\n2. Push Drake into Kanye \n\n3. Run away");
 
       if (answer1_2_2 === "1" || answer1_2_2 === "Engage Kanye in hand to hand combat") {
         score -= score;
@@ -110,7 +110,7 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
 
     } else if (answer1_2 === "3" || answer1_2 === "Find T-Swift and see if she's dating anyone right now") {
       score += 1;
-      var answer1_2_3 = prompt("You manage to track T-Swift down and start flirting your hardest. She seems into you, but then Kanye approaches. 'Yo Taylor, I'mma let you finish, but first I gotta deal with this traitor.' Do you 1. Apologize and appeal to Kanye's mercy 2. Pretend he's not there 3. Run!");
+      var answer1_2_3 = prompt("You manage to track T-Swift down and start flirting your hardest. She seems into you, but then Kanye approaches. 'Yo Taylor, I'mma let you finish, but first I gotta deal with this traitor.' Do you \n\n1. Apologize and appeal to Kanye's mercy \n\n2. Pretend he's not there \n\n3. Run!");
 
       if (answer1_2_3 === "1" || answer1_2_3 === "Apologize and appeal to Kanye's mercy") {
         score += 5;
@@ -135,11 +135,11 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
 
 } else if (answer1 === "3" || answer1 === "Go get some food") {
   score += 1;
-  var answer1_3 = prompt("'I am going to order the salmon medium instead of medium well. I don't want to ruin the magic,' says Kanye. You jump on the back of his crotch rocket and ride down to Fish and Shizz. You are seated in the VIP section and given a menu. What do you order? 1. The salmon 2. The filet mignon 3. Doritos and dom perignon");
+  var answer1_3 = prompt("'I am going to order the salmon medium instead of medium well. I don't want to ruin the magic,' says Kanye. You jump on the back of his crotch rocket and ride down to Fish and Shizz. You are seated in the VIP section and given a menu. What do you order? \n\n1. The salmon \n\n2. The filet mignon \n\n3. Doritos and dom perignon");
 
   if (answer1_3 === "1" || "The salmon") {
     score += 1;
-    var answer1_3_1 = prompt("'You can't copy Kanye!' says Kanye, and throws his drink in your face. 'You may be talented, but you're not Kanye West!' Do you 1. Get up and walk out 2. Dry your face and apologize 3. Flip the table");
+    var answer1_3_1 = prompt("'You can't copy Kanye!' says Kanye, and throws his drink in your face. 'You may be talented, but you're not Kanye West!' Do you \n\n1. Get up and walk out \n\n2. Dry your face and apologize \n\n3. Flip the table");
 
     if (answer1_3_1 === "1" || answer1_3_1 === "Get up and walk out") {
       score -= 1;
@@ -159,7 +159,7 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
 
   } else if (answer1_3 === "2" || "The filet mignon") {
     score += 1;
-    var answer1_3_2 = prompt("'You got beef with Kanye?' he asks. 'You wanna be startin' something with your food choice son?' Do you 1. Apologize. You don't want beef with Kanye 2. Say you just wanted some steak 3. Flip the table");
+    var answer1_3_2 = prompt("'You got beef with Kanye?' he asks. 'You wanna be startin' something with your food choice son?' Do you \n\n1. Apologize. You don't want beef with Kanye \n\n2. Say you just wanted some steak \n\n3. Flip the table");
 
     if (answer1_3_2 === "1" || "Apologize. You don't want beef with Kanye") {
       score += 5;
@@ -180,7 +180,7 @@ if (answer1 === "1" || answer1 === "Go to the bike store"){
 
   } else if (answer1_3 === "3" || "Doritos and dom perignon") {
     score += 1;
-    var answer1_3_3 = prompt("'You gonna order the Kanye in Kanye's favorite restaurant? That's bold son. I like you. You may ask me one question.' Do you ask 1. What he thinks of planes 2. What he thinks of fur pillows 3. What he thinks of apples");
+    var answer1_3_3 = prompt("'You gonna order the Kanye in Kanye's favorite restaurant? That's bold son. I like you. You may ask me one question.' Do you ask \n\n1. What he thinks of planes \n\n2. What he thinks of fur pillows \n\n3. What he thinks of apples");
 
     if (answer1_3_3 === "1" || answer1_3_3 === "What he thinks of planes") {
       score += 5;
