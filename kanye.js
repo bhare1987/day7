8217f397a2c19c706033aaa6af60fde9a6c74500
bhare1987 <brandonlhare@gmail.com>
2016-02-10 12:30:20 -0500

    initial commit

---
+++ kanye.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Jouney to the (Kanye) West!");
+var answer1 = prompt("Congratulations! You have been chosen to spend the day with Kanye West. This is a rare honor Mr. West bestows only upon his most loyal subjects. Mr. West sets the itinerary, but he gives you the choice. What do you want to do? 1. 2. 3.");
+if (answer1 === 1){
+
+} else if (answer1 === 2) {
+
+} else if (answer1 === 3) {
+
+} else {
+  console.log("Your unwillingness to play by Kanye's rules has angered Kanye. He takes his ball and goes home. You lose.");
+}
